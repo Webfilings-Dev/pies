249142ebecb9ae5f9ebad10bb5f174b8cdb554f3
Daniel Bruns <daniel.bruns@workiva.com>
2015-12-01 11:16:10 -0600

    Test 3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_test_requires():
 
 setup_args = dict(
     name='pies',
-    version='test-docs2',
+    version='test-docs3',
     packages=find_packages(),
     namespace_packages=['pies'],
     install_requires=get_install_requires(),
